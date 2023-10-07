a019f57c6cad2ce819b24bda1652f60c0adff639
Wiznat <wyattmccabe@gmail.com>
2023-10-06 20:16:31 -0700

    Not sure

--- GPTConvoEC2/ScriptBuilder.py
+++ GPTConvoEC2/ScriptBuilder.py
@@ -191,7 +191,7 @@ class ScriptObjects:
         "{name1} gets mad and threatens to throw them self in a wood chipper",
         "{name1} asks the other characters to drain his balls",
         "{name1} asks the other characters to milk him",
-        "{name1} to please kill him",
+        "{name1} asks the others to please kill him",
     ]
 
     funny_topics_two = [
